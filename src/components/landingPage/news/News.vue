2a4b3c3190c8dcef82bfb2e1c1ac64afee2b7ad3
LnrdRmj <leonardo.ra14@icloud.com>
2024-07-22 13:23:26 +0200

    mobile news section

--- src/components/landingPage/news/News.vue
+++ src/components/landingPage/news/News.vue
@@ -6,8 +6,9 @@ const props = defineProps<{ news: News[] }>()
 </script>
 
 <template>
-    <div class="flex bg-stone-100 overflow-x-scroll snap-x">
-        <div class="w-1/3 p-10 flex flex-col shrink-0 snap-center relative
+    <div class="h-fit flex bg-stone-100
+        overflow-y-scroll sm:overflow-x-scroll snap-x" ref="container">
+        <div class="w-11/12 sm:w-1/3 p-10 sm:p-10 flex flex-col shrink-0 snap-center relative
             after:bg-neutral-200 after:my-10 after:h-4/5 after:w-[1.5px] after:absolute after:right-0 after:top-0
             last:after:hidden" v-for="singleNews of news">
             <div class="text-sm bg-stone-200 w-fit px-2 rounded-md font-medium">{{ singleNews.date }}</div>
