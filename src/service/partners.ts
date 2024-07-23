fb516413714c328a6f6db5cebdbb5189c64d7905
LnrdRmj <leonardo.ra14@icloud.com>
2024-07-23 11:44:25 +0200

    update partners data

--- src/service/partners.ts
+++ src/service/partners.ts
@@ -1,5 +1,27 @@
 import EuWorkLogo from "../../src/components/landingPage/assets/partnerLogos/EuWork.png";
-import ListoneGiordano from "../../src/components/landingPage/assets/partnerLogos/Listone Giordano.png";
-import TecnoGrafica from "../../src/components/landingPage/assets/partnerLogos/Tecno Grafica.png";
+import ListoneGiordanoLogo from "../../src/components/landingPage/assets/partnerLogos/Listone Giordano.png";
+import TecnoGraficaLogo from "../../src/components/landingPage/assets/partnerLogos/Tecno Grafica.png";
 
-export const partnerLogos = [ListoneGiordano, TecnoGrafica, EuWorkLogo];
+import EuWorkGrayLogo from "../../src/components/landingPage/assets/partnerLogos/gray/EuWork.png";
+import ListoneGiordanoGrayLogo from "../../src/components/landingPage/assets/partnerLogos/gray/Listone Giordano.png";
+import TecnoGraficaGrayLogo from "../../src/components/landingPage/assets/partnerLogos/gray/Tecno Grafica.png";
+
+export const partnerLogos = [ListoneGiordanoLogo, TecnoGraficaLogo, EuWorkLogo];
+
+export const partnersData = [
+  {
+    name: "Eu Work",
+    logo: EuWorkLogo,
+    grayLogo: EuWorkGrayLogo,
+  },
+  {
+    name: "Listone Giordano",
+    logo: ListoneGiordanoLogo,
+    grayLogo: ListoneGiordanoGrayLogo,
+  },
+  {
+    name: "Tecno Grafica",
+    logo: TecnoGraficaLogo,
+    grayLogo: TecnoGraficaGrayLogo,
+  },
+];
